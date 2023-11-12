fe1ae35b2eafe112df19d20cea58a260e6f6474c
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-11-13 00:26:29 +0100

    setting up environment & dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
