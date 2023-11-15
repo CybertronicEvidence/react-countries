dbb3ccfabf818264e7457e95dd94800100bc2176
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-11-15 04:41:27 +0100

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,29 @@
-import PropTypes from "prop-types";
-
-const Header = (props) => {
-  // const { welcome, title, subtitle } = props;
-  console.log(props);
-  return <header></header>;
+const Header = () => {
+  let links = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "/" },
+    { name: "Contact", link: "/" },
+    { name: "Login", link: "/" },
+    { name: "Register", link: "/" },
+  ];
+  return (
+    <header className="p-4 shadow-lg">
+      <div className="p-10 sm:flex items-center justify-between py-4 md:px-10 px-7">
+        <div className=" font-bold text-2xl cursor-pointer flex items-center ">
+          React
+        </div>
+        <ul className="sm:flex sm:items-center">
+          {links.map((link) => (
+            <li key={link.name} className="sm:ml-8 text-lg">
+              <a href={link.link} className="hover:text-gray-400 duration-500">
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </header>
+  );
 };
 
-Header.propTypes = {
-  data: {
-    welcome: PropTypes.string,
-    title: PropTypes.string,
-    subtitle: PropTypes.string,
-  },
-};
 export default Header;
