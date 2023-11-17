9d11ca6551d603edb35129ec962e235d4803b89e
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-11-17 09:09:06 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{html,js,jsx,ts,tsx}',
+    './src/components/**/*.{html,js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
