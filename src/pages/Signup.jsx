8c9ec949e430862d597f68f771185a5a26d724aa
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-11-18 05:13:48 +0100

    Signup form

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import { SignupForm } from "../components/Form";
+import Header from "../components/Header";
+
+const Signup = () => {
+  return (
+    <>
+      <Header />
+      <SignupForm />
+    </>
+  );
+};
+
+export default Signup;
