9a8c18316b93144a67ec2f76f0fce91c11561f84
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-11-25 07:11:31 +0100

    Router changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Home from "./pages/Home";
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
       </Routes>
-    </>
+    </Router>
   );
 };
 
