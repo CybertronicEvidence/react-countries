ce86992f99c0e1441b5ab487d40860e151735f43
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-11-25 07:43:20 +0100

    Add dashboard to path

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
+        <Route path="dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
