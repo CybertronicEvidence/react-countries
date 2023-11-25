1c38fd4782954463371c0607790318cccd2cb665
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-11-25 07:42:58 +0100

    Dashboard page

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Profile from "../components/Profile";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      <Profile />
+    </>
+  );
+};
+
+export default Dashboard;
